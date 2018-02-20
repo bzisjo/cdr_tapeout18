1b479db4642b76256c790bbc0f8b006a54a82934
Jonathan Wang <bzisjo@gmail.com>
2018-02-19 16:32:53 -0800

    ready for compilation attempt

--- src/main/scala/cdr/CDR.scala
+++ src/main/scala/cdr/CDR.scala
@@ -68,6 +68,7 @@ class CDR(adc_width: Int = 5, space_counter_width: Int = 5, IF_value: Int = 15, 
   val shiftreg_ptr = RegInit(0.U(log2Up(shift_bits).W))   // Initialized to point to noclk_shiftreg(0.U)
   val last_bit = RegInit(0.U(1.W))
   val recovered_bit = Wire(UInt(1.W))
+  val backup_sum_bit = RegInit(0.U(1.W))
   val first_cycle_done = RegInit(0.U(1.W))
 
   sym_period_counter := Mux(!start_CR || sym_period_counter === (shift_bits-1).U,
@@ -99,6 +100,8 @@ class CDR(adc_width: Int = 5, space_counter_width: Int = 5, IF_value: Int = 15, 
   last_bit := Mux(sym_period_counter === 0.U, (data_sum > 0.U).asUInt, last_bit)          // This is a register to store the last bit
   recovered_bit := Mux(sym_period_counter === 0.U, (data_sum > 0.U).asUInt, last_bit)     // This is a wire that outputs the last recovered bit at the right clock period
 
+  backup_sum_bit := Mux(sym_period_counter === 0.U, (backup_sum > 0.U).asUInt, backup_sum_bit)
+
   // These commented lines contain an incomplete clock deviation correction algorithm
   val extra_bit = RegInit(0.U(1.W))
   val extra_pause = RegInit(0.U(1.W))
@@ -106,19 +109,17 @@ class CDR(adc_width: Int = 5, space_counter_width: Int = 5, IF_value: Int = 15, 
   first_cycle_done := first_cycle_done || sym_period_counter === (shift_bits-1).U         // Don't want to trigger following logic before first cycle
 
   when (first_cycle_done && sym_period_counter === 0.U) { // start_CR is implied
+    extra_bit := 0.U
+    extra_pause := 0.U
     when ((data_sum > 0.U) && (mid_sum > data_sum - mid_sum) || (data_sum <= 0.U) && (mid_sum < data_sum - mid_sum)) {
       shiftreg_ptr := shiftreg_ptr + CR_adjust_res.U
       when (shiftreg_ptr + CR_adjust_res.U > (shift_bits-1).U) {
         extra_bit := 1.U                                  // Need to output what's already in noclk_shiftreg
-      } .otherwise {
-        extra_bit := 0.U                                  // Reset flag
       }
     } .elsewhen ((data_sum > 0.U) && (mid_sum < data_sum - mid_sum) || (data_sum <= 0.U) && (mid_sum > data_sum - mid_sum)) {
       shiftreg_ptr := shiftreg_ptr - CR_adjust_res.U
       when (shiftreg_ptr < CR_adjust_res.U) {
         extra_pause := 1.U                                // Need to not output anything for one symbol length, since data in noclk_shiftreg is already outputted
-      } .otherwise {
-        extra_pause := 0.U                                // Reset flag
       }
     }
   }
@@ -127,13 +128,18 @@ class CDR(adc_width: Int = 5, space_counter_width: Int = 5, IF_value: Int = 15, 
   val s_idle :: s_regular :: s_extra_bit :: s_extra_pause :: Nil = Enum(4)
   val state = Reg(init = s_idle)
 
+  io.data_out.bits := 0.U
+  io.data_out.valid := false.B
+
   when (state === s_idle) {
     // Next-state logic
     when (first_cycle_done && sym_period_counter === 0.U) {state := s_regular}
 
     // Current state output
-    // TODO
+    io.data_out.bits := 0.U
+    io.data_out.valid := false.B
   }
+
   when (state === s_regular) {
     // Next-state logic
     when (io.data_out.ready) {
@@ -143,21 +149,24 @@ class CDR(adc_width: Int = 5, space_counter_width: Int = 5, IF_value: Int = 15, 
     }
 
     // Current state output
-    io.data_out.bits := 0.U
-    io.data_out.valid := 
+    io.data_out.bits := recovered_bit
+    io.data_out.valid := true.B
   }
+
   when (state === s_extra_bit) {
     // Next-state logic
     when (io.data_out.ready) {state := s_idle}
 
     // Current state output
+    io.data_out.bits := backup_sum_bit
+    io.data_out.valid := true.B
   }
   when (state === s_extra_pause) {
     // Next-state logic
-    when (io.data_out.ready) {state := s_idle}
+    when (sym_period_counter === 0.U) {state := s_idle}
 
     // Current state output
+    io.data_out.bits := recovered_bit
+    io.data_out.valid := false.B
   }
-
-
 }
